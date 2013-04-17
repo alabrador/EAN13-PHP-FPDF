bd3dc4d0f8953a32d1fd65ab8ca90b8020651ae8
Alexander Labrador <globehackers@gmail.com>
2013-04-17 16:39:42 -0430

    update

--- index.php
+++ index.php
@@ -24,8 +24,7 @@
 <body>
 	<div id="container">
         <div id="contact-form" class="clearfix">
-            <h1>Get In Touch!</h1>
-            <h2>Fill out our super swanky HTML5 contact form below to get in touch with us! Please provide as much information as possible for us to help you with your enquiry :)</h2>
+            <h1>Imprimir etiquetas</h1>
             <?php
 			//init variables
 			$cf = array();
@@ -37,7 +36,7 @@
 			}
             ?>
             <ul id="errors" class="<?php echo ($sr && !$cf['form_ok']) ? 'visible' : ''; ?>">
-                <li id="info">There were some problems with your form submission:</li>
+                <li id="info">Hubo algunos problemas con el envío del formulario:</li>
                 <?php 
 				if(isset($cf['errors']) && count($cf['errors']) > 0) :
 					foreach($cf['errors'] as $error) :
@@ -48,30 +47,21 @@
 				endif;
 				?>
             </ul>
-            <p id="success" class="<?php echo ($sr && $cf['form_ok']) ? 'visible' : ''; ?>">Thanks for your message! We will get back to you ASAP!</p>
-            <form method="post" action="process.php">
-                <label for="name">Name: <span class="required">*</span></label>
-                <input type="text" id="name" name="name" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['name'] : '' ?>" placeholder="John Doe" required autofocus />
-                
-                <label for="email">Email Address: <span class="required">*</span></label>
-                <input type="email" id="email" name="email" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['email'] : '' ?>" placeholder="johndoe@example.com" required />
-                
-                <label for="telephone">Telephone: </label>
-                <input type="tel" id="telephone" name="telephone" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['telephone'] : '' ?>" />
-                
-                <label for="enquiry">Enquiry: </label>
-                <select id="enquiry" name="enquiry">
-                    <option value="General" <?php echo ($sr && !$cf['form_ok'] && $cf['posted_form_data']['enquiry'] == 'General') ? "selected='selected'" : '' ?>>General</option>
-                    <option value="Sales" <?php echo ($sr && !$cf['form_ok'] && $cf['posted_form_data']['enquiry'] == 'Sales') ? "selected='selected'" : '' ?>>Sales</option>
-                    <option value="Support" <?php echo ($sr && !$cf['form_ok'] && $cf['posted_form_data']['enquiry'] == 'Support') ? "selected='selected'" : '' ?>>Support</option>
+            <p id="success" class="<?php echo ($sr && $cf['form_ok']) ? 'visible' : ''; ?>">Imprimiendo!</p>
+            <form method="post" action="etiq_pequena.php">
+                <label for="name">Codigo: <span class="required">*</span></label>
+                <input type="text" id="code1" name="code" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['code'] : '' ?>" placeholder="Ingrese codigo de etiqueta" required autofocus />
+         
+                <label for="tipo_etiq">Tipo de Etiqueta: </label>
+                <select id="tipo_etiq" name="tipo_etiq">
+                    <option value="Etiqueta Estandar" <?php echo ($sr && !$cf['form_ok'] && $cf['posted_form_data']['enquiry'] == 'estandar') ? "selected='selected'" : '' ?>>Etiqueta Estandar</option>
+                    <option value="Etiqueta Mediana" <?php echo ($sr && !$cf['form_ok'] && $cf['posted_form_data']['enquiry'] == 'mediana') ? "selected='selected'" : '' ?>>Etiqueta Mediana</option>
+                    <option value="Etiqueta pequeña" <?php echo ($sr && !$cf['form_ok'] && $cf['posted_form_data']['enquiry'] == 'pequena') ? "selected='selected'" : '' ?>>Etiqueta Pequeña</option>
                 </select>
                 
-                <label for="message">Message: <span class="required">*</span></label>
-                <textarea id="message" name="message" placeholder="Your message must be greater than 20 charcters" required data-minlength="20"><?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['message'] : '' ?></textarea>
-                
                 <span id="loading"></span>
-                <input type="submit" value="Holla!" id="submit-button" />
-                <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
+                <input type="submit" value="Imprimir" id="submit-button" />
+                <p id="req-field-desc"><span class="required">*</span> Indica los campos obligatorios</p>
             </form>
             <?php unset($_SESSION['cf_returndata']); ?>
         </div>
